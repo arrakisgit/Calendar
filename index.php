52dc8096d979dd0ae3ed62a9d0da7c5e939807a1
arrakis-dev <elouazzani.amine@gmail.com>
2018-01-31 04:49:30 +0100

    test

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 						</select>
 						<h4 class="rdv">Créer un Evenement</h4>
 						<div class="rdv">
-						<form name="frm_rdv" method="POST" action="/">
+						<form name="frm_rdv" method="POST" action="php/listener.php">
 						<label class="rdv">
 							<h5>Heure de début</h5>
 							<select id="hoursStart" name="debutselheure">
