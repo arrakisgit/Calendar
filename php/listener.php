6bd3115272a8e1de3acda09c3194572e7c159f33
arrakis-dev <elouazzani.amine@gmail.com>
2018-02-03 04:55:12 +0100

    test

--- php/listener.php
+++ php/listener.php
@@ -35,9 +35,17 @@ class Listener
 		$endminute=''.$_POST['endminute'];//+0;
 		$eventype=$_POST['eventtype'];
 		//echo "(0,".((int)$startminute).",".((int)$starthour).",".(((int)$selmonth)+1).",".((int)$selday).",".(((int)$selyear)+1900).")";
-		//$timestampstart=mktime(0,((int)$startminute),((int)$starthour),(((int)$selmonth)+1),((int)$selday),(((int)$selyear)+1900));
-		//$timestampend=mktime(0,((int)$endminute),((int)$endhour),(((int)$selmonth)+1),((int)$selday),(((int)$selyear)+1900));
 		
+		
+		 $st_min=((int)$startminute);
+		 $st_hr=((int)$starthour);
+		 $en_min=((int)$endminute);
+		 $en_hr=((int)$endhour);
+		 $intmonth=(((int)$selmonth)+1);
+		 $intday=((int)$selday);
+		 $intyear=(((int)$selyear)+1900);
+		 $timestampstart=mktime(0,$st_min,$st_hr,$intmonth,$intday,$intyear);
+		 $timestampend=mktime(0,$en_min,$en_hr,$intmonth,$intday,$intyear);
 		//$oDAO = new DAO();
 		//$oDAO->Insert_Event($title,$timestampstart,$timestampend,$eventype);
 		echo "test_";//$timestampstart;
